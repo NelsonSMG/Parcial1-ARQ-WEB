fa376bbf9cb557fa1643127cd0eacc6832dcd154
Warxwell <senhorpompas@gmail.com>
2021-10-30 19:54:45 -0300

    Reglas - Carga de pagina a partir de respuesta

--- js/regla.js
+++ js/regla.js
@@ -18,9 +18,25 @@ $(document).ready(function () {
         contentType: "application/json; charset=utf-8", 
         dataType: 'json', // added data type
         success: function(res) {
-            console.log("Peticion funcional");
-            console.log(res);
-            alert(res);
+            //console.log("Peticion funcional");
+            //console.log(res);
+
+            $.each(res, function(i, f){
+                var tableRow = "<tr>"+
+                            "<th scope='row'>"+f.id+"</th>" +
+                            "<td>"+f.limiteMin+"</td>" +
+                            "<td>"+f.limiteMax+"</td>" +
+                            "<td>"+f.montoEquivalencia+"</td>" +
+                            "<td>1</td>" +
+                            "<td>" + 
+                              "<button class='btn btn-warning' data-toggle='modal' data-target='#exampleModal'>Editar</button> &nbsp" +
+                              "<button class='btn btn-danger' data-toggle='modal' data-target='#deleteModal'>Eliminar</button>" +
+                            "</td>" +
+                          "</tr>";
+
+                $(tableRow).appendTo("#rulesTable");
+            });
+
         }
     });
 
