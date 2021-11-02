ef4661e72e570eb97cddf3bd8eb8cb3b1866e571
Eduardo Maciel <tachi.maciel98@gmail.com>
2021-11-01 22:59:55 -0300

    Conexion GET-POST-DELETE  cancepto canje

--- js/concepto.js
+++ js/concepto.js
@@ -1,14 +1,11 @@
 
 var action = "";
 
-function cargarDatos(){
+
+function loadConcept(){
+
     console.log("Cargar conceptos de puntos");
-};
-
-
-$(document).ready(function () {
-
-    console.log("Pagina de conceptos");
+    $("#conceptTable").empty();
 
     $.ajax({
         method: "GET",
@@ -19,13 +16,13 @@ $(document).ready(function () {
         dataType: 'json', 
         success: function(res) {
             $.each(res, function(i, f){
-                var tableRow = "<tr>"+
+                var tableRow = "<tr id="+f.id+" >"+
                             "<th scope='row'>"+f.id+"</th>" +
                             "<td>"+f.descConcepto+"</td>" +     
                             "<td>"+f.puntosRequeridos+"</td>" +
                             "<td>" + 
                               "<button class='btn btn-warning' data-toggle='modal' data-target='#exampleModal'>Editar</button> &nbsp" +
-                              "<button class='btn btn-danger' data-toggle='modal' data-target='#deleteModal'>Eliminar</button>" +
+                              "<button class='btn btn-danger delete' data-toggle='modal' data-target='#deleteModal'>Eliminar</button>" +
                             "</td>" +
                           "</tr>";
 
@@ -35,4 +32,69 @@ $(document).ready(function () {
         }
     });
 
+}
+$(document).ready(function () {
+
+    console.log("Pagina de reglas");
+
+    loadConcept();
+
+    $('#new').on('click', function (e) {
+        console.log("Nuevo");
+        action = "new"
+        
+    });
+
+    $(document).on('click', '.delete', function (e) {
+        console.log("Eliminar");
+        var id = $(e.target).closest("tr").attr('id');
+        console.log(id);        
+
+        $.ajax({
+            method: "DELETE",
+            type: "DELETE",
+            url: "http://localhost:8080/fidelus/concepto-canje/"+id,
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8", 
+            dataType: 'json', // added data type
+            success: function(res) {
+                console.log("DELETE funcional");
+                loadConcept();
+            },
+            error: function () {
+                loadConcept();
+            }
+        });
+
+
+    });
+
+    $('#guardar').on('click', function (e) {
+
+        console.log(action);
+
+        body = {
+            "descConcepto": document.getElementById('conceptoDesc').value,
+            "puntosRequeridos": document.getElementById('conceptPoint').value
+        }
+        $.ajax({
+            method: "POST",
+            type: "POST",
+            url: "http://localhost:8080/fidelus/concepto-canje",
+            data:  JSON.stringify(body),
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8", 
+            dataType: 'json', // added data type
+            success: function(res) {
+                console.log("POST funcional");
+                loadConcept();
+            },
+            error: function () {
+                loadConcept();
+            }
+        });
+
+    });
+
 });
+
