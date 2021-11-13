c593212e68edd22aef998f1c49448b99c4d5a1f0
Eduardo Maciel <tachi.maciel98@gmail.com>
2021-11-13 15:30:28 -0300

    GET bolsa_puntos

---
+++ js/bolsa_puntos.js
@@ -0,0 +1,41 @@
+var action = "";
+
+
+function loadBolsa(){
+
+    console.log("Cargar bolsa de puntos");
+    $("#bolsaPuntos").empty();
+
+    $.ajax({
+        method: "GET",
+        type: "GET",
+        url: "http://localhost:8080/fidelus/bolsa-puntos",
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8", 
+        dataType: 'json', 
+        success: function(res) {
+            $.each(res, function(i, f){
+                var tableRow = "<tr id="+f.id+" >"+
+                            "<th scope='row'>"+f.id+"</th>" +
+                            "<td>"+f.idCliente+"</td>" +     
+                            "<td>"+f.fechaAsignacion+"</td>" +
+                            "<td>"+f.fechaVencimiento+"</td>" +     
+                            "<td>"+f.puntajeAsignado+"</td>" +   
+                            "<td>"+f.puntajeUtilizado+"</td>" +
+                            "<td>"+f.saldo+"</td>" +
+                            "<td>"+f.monto+"</td>" +
+                          "</tr>";
+
+                $(tableRow).appendTo("#bolsaPuntos");
+            });
+
+        }
+    });
+
+}
+$(document).ready(function () {
+
+    console.log("Pagina de bolsa");
+
+    loadBolsa();
+});
