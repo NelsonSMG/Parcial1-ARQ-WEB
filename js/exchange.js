96059bf0581bc555063e7d01589fb6354d243f50
Warxwell <senhorpompas@gmail.com>
2021-11-14 11:45:59 -0300

    Canje - Carga dinánica de página de canje, modal para canje y nueva pagina de canje

---
+++ js/exchange.js
@@ -0,0 +1,75 @@
+
+var action = "";
+
+
+function loadConcept(){
+
+    console.log("Cargar conceptos de puntos");
+    $("#conceptTable").empty();
+
+    $.ajax({
+        method: "GET",
+        type: "GET",
+        url: "http://localhost:8080/fidelus/concepto-canje",
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8", 
+        dataType: 'json', 
+        success: function(res) {
+            $.each(res, function(i, f){
+                var tableRow = "<div class='col-lg-4'>"+
+                                "<div class='card' id="+f.id+" >"+
+                                    "<div class='card-header'>" +
+                                        "<h2 class='display h4'> Producto "+f.id+"</h2>" +     
+                                    "</div>" +
+                                    "<div class='card-body d-flex flex-column'>" + 
+                                        "<div class='row d-flex'>"+ 
+                                            "<div class='col-sm-6'>"+
+                                                "<img src="+"img\\mockup6.jpg"+" class='img-fluid' alt='Imagen'>"+
+                                            "</div>" +
+                                            "<div class='col-sm-6'>"+
+                                                "<h3>"+f.descConcepto+"</h3>"+
+                                                "<p>Descripción producto</p>"+
+                                                "<p>"+f.puntosRequeridos+" Puntos</p>"+
+                                                "<button class='btn btn-primary mt-auto btn-block' data-toggle='modal' data-target='#exampleModal'>Canjear</button>" +
+                                            "</div>" +
+                                        "</div>" +
+                                    "</div>" +
+                                "</div>" +
+                          "</div>";
+
+                $(tableRow).appendTo("#exchangeRow");
+            });
+
+        }
+    });
+
+}
+$(document).ready(function () {
+
+    loadConcept();
+
+    $(document).on('click', '.exchange', function (e) {
+        console.log("Canjear");
+       
+        var client= document.getElementById('clientId').value;
+        var concept= document.getElementById('concept').value;
+
+        $.ajax({
+            method: "POST",
+            type: "POST",
+            url: "http://localhost:8080/fidelus/canje-puntos/canjear?idConceptoCanje="+concept+"&idCliente="+client,
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8", 
+            dataType: 'json', // added data type
+            success: function(res) {
+                console.log("POST funcional");
+            },
+            error: function () {
+            }
+        });
+
+
+    });
+
+});
+
