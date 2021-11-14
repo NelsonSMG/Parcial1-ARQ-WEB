32ee9ecad64d13a90343c3208cff67ab57bac936
Warxwell <senhorpompas@gmail.com>
2021-11-14 12:11:30 -0300

    Canje de puntos Funcional

--- js/exchange.js
+++ js/exchange.js
@@ -1,5 +1,6 @@
 
 var action = "";
+var conceptId;
 
 
 function loadConcept(){
@@ -17,7 +18,7 @@ function loadConcept(){
         success: function(res) {
             $.each(res, function(i, f){
                 var tableRow = "<div class='col-lg-4'>"+
-                                "<div class='card' id="+f.id+" >"+
+                                "<div class='card'>"+
                                     "<div class='card-header'>" +
                                         "<h2 class='display h4'> Producto "+f.id+"</h2>" +     
                                     "</div>" +
@@ -26,11 +27,11 @@ function loadConcept(){
                                             "<div class='col-sm-6'>"+
                                                 "<img src="+"img\\mockup6.jpg"+" class='img-fluid' alt='Imagen'>"+
                                             "</div>" +
-                                            "<div class='col-sm-6'>"+
+                                            "<div class='col-sm-6' id="+f.id+" >"+
                                                 "<h3>"+f.descConcepto+"</h3>"+
                                                 "<p>Descripción producto</p>"+
                                                 "<p>"+f.puntosRequeridos+" Puntos</p>"+
-                                                "<button class='btn btn-primary mt-auto btn-block' data-toggle='modal' data-target='#exampleModal'>Canjear</button>" +
+                                                "<button class='btn btn-primary mt-auto btn-block concept' data-toggle='modal' data-target='#exampleModal'>Canjear</button>" +
                                             "</div>" +
                                         "</div>" +
                                     "</div>" +
@@ -48,7 +49,15 @@ $(document).ready(function () {
 
     loadConcept();
 
-    $(document).on('click', '.exchange', function (e) {
+    $(document).on('click', '.concept', function (e) {
+
+        conceptId = $(e.target).closest("div").attr('id');
+        console.log(conceptId);
+        document.getElementById('concept').value = conceptId;
+       
+    });
+
+    $(document).on('click', '#exchange', function (e) {
         console.log("Canjear");
        
         var client= document.getElementById('clientId').value;
@@ -63,8 +72,10 @@ $(document).ready(function () {
             dataType: 'json', // added data type
             success: function(res) {
                 console.log("POST funcional");
+                $('#exampleModal').modal('hide');
             },
             error: function () {
+                $('#exampleModal').modal('hide');
             }
         });
 
