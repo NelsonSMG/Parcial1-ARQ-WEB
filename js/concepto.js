9cd9e06e26b23964ff8feac5241d6bfc096ac519
Eduardo Maciel <tachi.maciel98@gmail.com>
2021-11-18 20:10:51 -0300

    Metodo PUT-Concepto de puntos archivo JS

--- js/concepto.js
+++ js/concepto.js
@@ -1,6 +1,8 @@
 
 var action = "";
-
+var rowId;
+var rowdesConcepto;
+var rowpuntosRequeridos;
 
 function loadConcept(){
 
@@ -18,10 +20,10 @@ function loadConcept(){
             $.each(res, function(i, f){
                 var tableRow = "<tr id="+f.id+" >"+
                             "<th scope='row'>"+f.id+"</th>" +
-                            "<td>"+f.descConcepto+"</td>" +     
-                            "<td>"+f.puntosRequeridos+"</td>" +
+                            "<td class='descConcepto'>"+f.descConcepto+"</td>" +     
+                            "<td class='puntosRequeridos'>"+f.puntosRequeridos+"</td>" +
                             "<td>" + 
-                              "<button class='btn btn-warning' data-toggle='modal' data-target='#exampleModal'>Editar</button> &nbsp" +
+                              "<button class='btn btn-warning edit' data-toggle='modal' data-target='#exampleModal'>Editar</button> &nbsp" +
                               "<button class='btn btn-danger delete' data-toggle='modal' data-target='#deleteModal'>Eliminar</button>" +
                             "</td>" +
                           "</tr>";
@@ -35,14 +37,14 @@ function loadConcept(){
 }
 $(document).ready(function () {
 
-    console.log("Pagina de reglas");
+    console.log("Pagina de conceptos");
 
     loadConcept();
 
     $('#new').on('click', function (e) {
-        console.log("Nuevo");
         action = "new"
-        
+        document.getElementById('conceptoDesc').value = "";
+        document.getElementById('conceptPoint').value = "";
     });
 
     $(document).on('click', '.delete', function (e) {
@@ -68,31 +70,78 @@ $(document).ready(function () {
 
 
     });
+    $(document).on('click', '.edit', function (e) {
+        console.log("Editar");
+        action = "edit"
+
+        //se guardan los datos del registro
+        rowId = $(e.target).closest("tr").attr('id');
+        rowdesConcepto = $(e.target).closest("tr").find(".descConcepto").html();
+        rowpuntosRequeridos = $(e.target).closest("tr").find(".puntosRequeridos").html();
+        
+        
+
+        console.log(rowId);
+        console.log(rowdesConcepto);
+        console.log(rowpuntosRequeridos);
+        /* */
+
+        //se cargan los datos en el modal
+        document.getElementById('conceptoDesc').value = rowdesConcepto;
+        document.getElementById('conceptPoint').value = rowpuntosRequeridos;
+    });
 
     $('#guardar').on('click', function (e) {
 
         console.log(action);
-
-        body = {
-            "descConcepto": document.getElementById('conceptoDesc').value,
-            "puntosRequeridos": document.getElementById('conceptPoint').value
-        }
-        $.ajax({
-            method: "POST",
-            type: "POST",
-            url: "http://localhost:8080/fidelus/concepto-canje",
-            data:  JSON.stringify(body),
-            crossDomain: true,
-            contentType: "application/json; charset=utf-8", 
-            dataType: 'json', // added data type
-            success: function(res) {
-                console.log("POST funcional");
-                loadConcept();
-            },
-            error: function () {
-                loadConcept();
+        if (action == "new") {
+            console.log("confirmar nuevo registro");
+            body = {
+                "descConcepto": document.getElementById('conceptoDesc').value,
+                "puntosRequeridos": document.getElementById('conceptPoint').value
             }
-        });
+            $.ajax({
+                method: "POST",
+                type: "POST",
+                url: "http://localhost:8080/fidelus/concepto-canje",
+                data:  JSON.stringify(body),
+                crossDomain: true,
+                contentType: "application/json; charset=utf-8", 
+                dataType: 'json', // added data type
+                success: function(res) {
+                    console.log("POST funcional");
+                    loadConcept();
+                },
+                error: function () {
+                    loadConcept();
+                }
+            });
+        }else{
+            console.log("confirmar edicion en "+rowId);
+
+            body = {
+                "descConcepto": document.getElementById('conceptoDesc').value,
+                "puntosRequeridos": document.getElementById('conceptPoint').value
+            }
+    
+            $.ajax({
+                method: "PUT",
+                type: "PUT",
+                url: "http://localhost:8080/fidelus/concepto-canje/"+rowId,
+                data:  JSON.stringify(body),
+                crossDomain: true,
+                contentType: "application/json; charset=utf-8", 
+                dataType: 'json', // added data type
+                success: function(res) {
+                    console.log("PUT funcional");
+                    loadConcept();
+                },
+                error: function () {
+                    loadConcept();
+                }
+            }); /* */
+           
+        }
 
     });
 
